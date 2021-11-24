a973707099067d113c27116b169a40a28d04ae46
Aaron Zhang <a.zhang1997@outlook.com>
2021-11-24 18:04:09 -0500

    added education summaries

--- aaron-portfolio/src/info.jsx
+++ aaron-portfolio/src/info.jsx
@@ -26,12 +26,12 @@ let info = {
         {
             "title": "UNC Chapel Hill",
             "subtitle": "B.A. in Computer Science",
-            "summary": "Some text."
+            "summary": "I dabbled in biology for two years before realizing that I was severely unhappy. In my junior year, I decided to confront the irritation of choosing my parent's preferred career path, Computer Science. Fortunately, since then, I've refound my inherent love of building. Switching to CS was probably the most pivotal decision of my young adult life, so I am excited to see where it takes me."
         },
         {
             "title": "UNC Chapel Hill",
             "subtitle": "Minor in Music",
-            "summary": "Some text."
+            "summary": "Music was my greatest passion in highschool. I came into university wanted to explore my musical path as much as possible. The Music Minor was a nice introduction to the study of music and what a life in music would entail. Ultimately, I decided it wasn't for me, but I hope to keep music in my life for as long as I can."
         }
     ],
     "Projects": [
