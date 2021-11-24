d38509c20e1fd0c84ca74f4398b4cd200a821c90
Aaron Zhang <a.zhang1997@outlook.com>
2021-11-24 01:28:27 -0500

    added navbar auto scroll

--- aaron-portfolio/src/App.js
+++ aaron-portfolio/src/App.js
@@ -9,12 +9,20 @@ function App() {
     return (
         <div class="vh-auto bg-dark text-white">
             {Navbar()}
-            {Title()}
-            {AboutMe("About Me", info["About Me"])}
-            {InfoContainer("Experience", info.Experience)}
-            {InfoContainer("Education", info.Education)}
-            {InfoContainer("Projects", info.Projects)}
-            {/*add contact section*/}
+            <div
+                data-bs-spy="scroll"
+                data-bs-target="#navbar"
+                data-bs-offset="0"
+                class="scrollspy-example"
+                tabindex="0"
+            >
+                {Title()}
+                {AboutMe("About Me", info["About Me"])}
+                {InfoContainer("Experience", info.Experience)}
+                {InfoContainer("Education", info.Education)}
+                {InfoContainer("Projects", info.Projects)}
+                {/*add contact section*/}
+            </div>
         </div>
     );
 }
--- aaron-portfolio/src/components/Navbar/navbar.jsx
+++ aaron-portfolio/src/components/Navbar/navbar.jsx
@@ -2,15 +2,15 @@ function Navbar () {
     return (
         <nav
             class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
-            aria-label="Tenth navbar example"
+            aria-label="navbar"
         >
             <div class="container-fluid">
                 <button
                     class="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
-                    data-bs-target="#navbarsExample08"
-                    aria-controls="navbarsExample08"
+                    data-bs-target="#navbars"
+                    aria-controls="navbars"
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
@@ -19,36 +19,36 @@ function Navbar () {
 
                 <div
                     class="collapse navbar-collapse justify-content-md-center"
-                    id="navbarsExample08"
+                    id="navbars"
                 >
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">
+                            <a class="nav-link active" aria-current="page" href="#Welcome">
                                 Welcome
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">
+                            <a class="nav-link" aria-current="page" href="#About Me">
                                 About me
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="#Experience">
                                 Experience
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="#Education">
                                 Education
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="#Projects">
                                 Projects
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="#Contact">
                                 Contact
                             </a>
                         </li>
