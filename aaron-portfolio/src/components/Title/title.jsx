49e478899de1ac976673ef5600895caf6ca2ef88
Aaron Zhang <a.zhang1997@outlook.com>
2021-11-24 01:00:18 -0500

    increased mt mb of title container

--- aaron-portfolio/src/components/Title/title.jsx
+++ aaron-portfolio/src/components/Title/title.jsx
@@ -2,7 +2,7 @@ import headshot from "./headshot3.jpg";
 
 function Title() {
     return (
-        <div class="container col-xxl-8 px-4 py-5">
+        <div class="container col-xxl-8 px-4 py-5 mt-5 mb-5">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div class="col-10 col-sm-8 col-lg-6">
                     <img
