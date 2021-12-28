17b8846255901c08fe600001240199a02250ea21
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2021-12-28 14:17:24 -0500

    Reduced title picture size

--- aaron-portfolio/src/components/Title/title.jsx
+++ aaron-portfolio/src/components/Title/title.jsx
@@ -1,26 +1,26 @@
 import headshot from "./headshot3.jpg";
 
 function Title() {
-    return (
-        <div class="container col-xxl-8 px-4 py-5 mt-5 mb-2" id="Welcome">
-            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-                <div class="col-10 col-sm-8 col-lg-6">
-                    <img
-                        src={headshot}
-                        class="d-block mx-lg-auto img-fluid rounded-circle shadow-lg"
-                        alt="Headshot"
-                        width="450"
-                        height="250"
-                        loading="lazy"
-                    ></img>
-                </div>
-                <div class="col-lg-6">
-                    <h1 class="display-2 fw-bold lh-1 mb-3">Hi! I'm Aaron.</h1>
-                    <p class="lead">Software engineer from Charlotte.</p>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div class="container col-xxl-8 px-4 py-5 mt-5 mb-2" id="Welcome">
+			<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+				<div class="col-10 col-sm-8 col-lg-6">
+					<img
+						src={headshot}
+						class="d-block mx-lg-auto img-fluid rounded-circle"
+						alt="Headshot"
+						width="350"
+						height="150"
+						loading="lazy"
+					></img>
+				</div>
+				<div class="col-lg-6">
+					<h1 class="display-2 fw-bold lh-1 mb-3">Hi! I'm Aaron.</h1>
+					<p class="lead">Software engineer from Charlotte.</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Title;
