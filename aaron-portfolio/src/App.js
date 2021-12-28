2e8697a17f8dde326fabea7d4bd832a60312e00a
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2021-12-28 16:18:41 -0500

    Updated info

--- aaron-portfolio/src/App.js
+++ aaron-portfolio/src/App.js
@@ -5,17 +5,17 @@ import info from "./info";
 import AboutMe from "./components/AboutMe/aboutMe";
 
 function App() {
-    return (
-        <div class="vh-auto bg-dark text-white">
-            {Navbar()}
-            {Title()}
-            {AboutMe("About Me", info["About Me"])}
-            {InfoContainer("Education", info.Education)}
-            {InfoContainer("Experience", info.Experience)}
-            {InfoContainer("Projects", info.Projects)}
-            {/*add contact section*/}
-        </div>
-    );
+	return (
+		<div class="vh-auto bg-dark text-white">
+			{Navbar()}
+			{Title()}
+			{AboutMe("About Me", info["About Me"])}
+			{InfoContainer("Education", info.Education)}
+			{InfoContainer("Experience", info.Experience)}
+			{InfoContainer("Projects", info.Projects)}
+			{/*add contact section*/}
+		</div>
+	);
 }
 
 export default App;
--- aaron-portfolio/src/info.jsx
+++ aaron-portfolio/src/info.jsx
@@ -1,52 +1,59 @@
 let info = {
-    "About Me": "Here is a little blurb about me!",
-    "Experience": [
-        {
-            "title": "Vanguard",
-            "subtitle": "Application Engineer I",
-            "summary": "Some text."
-        },
-        {
-            "title": "UNC Grounds",
-            "subtitle": "Frontend Engineer",
-            "summary": "Some text."
-        },
-        {
-            "title": "RENCI",
-            "subtitle": "Project Manager",
-            "summary": "Some text."
-        },
-        {
-            "title": "Second Main Phase",
-            "subtitle": "Software Engineer Intern",
-            "summary": "Some text."
-        }
-    ],
-    "Education": [
-        {
-            "title": "UNC Chapel Hill",
-            "subtitle": "B.A. in Computer Science",
-            "summary": "I dabbled in biology for two years before realizing that I was severely unhappy. In my junior year, I decided to confront the irritation of choosing my parent's preferred career path, Computer Science. Fortunately, since then, I've refound my inherent love of building. Switching to CS was probably the most pivotal decision of my early adult life, so I am excited to see where it takes me."
-        },
-        {
-            "title": "UNC Chapel Hill",
-            "subtitle": "Minor in Music",
-            "summary": "Music was my greatest passion in highschool. I came into university wanted to explore my musical path as much as possible. The Music Minor was a nice introduction to the study of music and what a life in music would entail. Ultimately, I decided it wasn't for me, but I hope to keep music in my life for as long as I can."
-        }
-    ],
-    "Projects": [
-        {
-            "title": "Fall Guys Stats",
-            "subtitle": "A Video Game Stat Tracker",
-            "summary": "FGS was my first web development project built using jQuery and Firebase. It was part of my web dev class and I worked on it with 3 close friends. 'Fall Guys' as a game was very popular at the time so we used the hype as a reason to build the site. Since then, the game has died down but the experience of building a multi-page data displaying website provided great experience, setting me up for later projects."
-        },
-        {
-            "title": "UNC Chapel Hill",
-            "subtitle": "Minor in Music",
-            "summary": "Some text."
-        }
-    ],
-    "Contact": []     
-}
+	"About Me": "Here is a little blurb about me!",
+	Experience: [
+		{
+			title: "Vanguard",
+			subtitle: "Application Engineer I",
+			summary:
+				"Vanguard is my first and current professional role out of school. My team and I own four API Microservices which we are currently working to bring to production.",
+		},
+		{
+			title: "UNC Grounds",
+			subtitle: "Frontend Engineer",
+			summary:
+				"My team and I owned and delivered a fully functional web app to provide the Grounds Department with a training website, Learnscaping.",
+		},
+		{
+			title: "RENCI",
+			subtitle: "Project Manager",
+			summary:
+				"I assisted my team as a project manager to deliver an automated meeting notes distribution service. This was my first opportunity to own and deliver a product in a self-driven development team.",
+		},
+		{
+			title: "Second Main Phase",
+			subtitle: "Software Engineer Intern",
+			summary:
+				"SMP was my first introduction to a professional development environment. I was able to push production code as well as develop internal resources in support of the product.",
+		},
+	],
+	Education: [
+		{
+			title: "UNC Chapel Hill",
+			subtitle: "B.A. in Computer Science",
+			summary:
+				"CS was my main area of study. However, before that, I spent two grueling years in Biology with my intentions of entering medicine. Although I ended up making a switch, my interest in life sciences remains. Perhaps one day I can find a way to tie in both fields of study.",
+		},
+		{
+			title: "UNC Chapel Hill",
+			subtitle: "Minor in Music",
+			summary:
+				"Music was my greatest passion in highschool. I came into university wanting to explore my musical path as much as possible. The Music Minor was a nice introduction to the study of music and what a life in music would entail. Ultimately, I decided it wasn't for me, but I hope to keep music in my life for as long as I can.",
+		},
+	],
+	Projects: [
+		{
+			title: "Fall Guys Stats",
+			subtitle: "A Video Game Stat Tracker",
+			summary:
+				"FGS was my first web app built with a team of close friends using jQuery and Firebase. The site has currently been taken down but it provided a lot of great lessons on web development that have greatly impacted me as a dev.",
+		},
+		{
+			title: "UNC Chapel Hill",
+			subtitle: "Minor in Music",
+			summary: "Some text.",
+		},
+	],
+	Contact: [],
+};
 
 export default info;
