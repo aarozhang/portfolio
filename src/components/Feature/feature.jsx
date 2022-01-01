7a906cffb8952faa1dee766606b8054fc73c49a4
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2022-01-01 13:07:29 -0500

    Added framer motion and applied it to details button

--- src/components/Feature/feature.jsx
+++ src/components/Feature/feature.jsx
@@ -1,17 +1,21 @@
-function Feature (organization, role, summary) {
-    return (
-        <div class="feature col">
-            <div class="feature-icon bg-primary bg-gradient"></div>
-            <h2>{organization}</h2>
-            <p class="lead">{role}</p>
-            <p>
-                {summary}
-            </p>
-            <a href="#" class="icon-link">
-                See details
-            </a>
-        </div>
-    );
+import { motion } from "framer-motion";
+
+function Feature(organization, role, summary) {
+	return (
+		<div class="feature col">
+			<div class="feature-icon bg-primary bg-gradient"></div>
+			<h2>{organization}</h2>
+			<p class="lead">{role}</p>
+			<p>{summary}</p>
+			<motion.button
+				whileHover={{ scale: 1.1 }}
+				whileTap={{ scale: 0.9 }}
+				onClick={() => null}
+			>
+				See details
+			</motion.button>
+		</div>
+	);
 }
 
 export default Feature;
