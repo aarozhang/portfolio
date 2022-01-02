9b3d26be98b479e64324ac1b4f1d088d2a80e26d
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2022-01-02 00:12:00 -0500

    Added title fade in animation

--- src/components/Title/title.jsx
+++ src/components/Title/title.jsx
@@ -1,10 +1,16 @@
 import headshot from "./headshot3.jpg";
+import { motion } from "framer-motion";
 
 function Title() {
 	return (
 		<div class="container col-xxl-8 px-4 py-5 mt-5 mb-2" id="Welcome">
-			<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-				<div class="col-10 col-sm-8 col-lg-6">
+			<motion.div
+				class="row flex-lg-row-reverse align-items-center g-5 py-5"
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 1.2, duration: 2 }}
+			>
+				<motion.div class="col-10 col-sm-8 col-lg-6">
 					<img
 						src={headshot}
 						class="d-block mx-lg-auto img-fluid rounded-circle"
@@ -13,12 +19,12 @@ function Title() {
 						height="175"
 						loading="lazy"
 					></img>
-				</div>
+				</motion.div>
 				<div class="col-lg-6">
 					<h1 class="display-2 fw-bold lh-1 mb-3">Hi! I'm Aaron.</h1>
 					<p class="lead">Software engineer from Charlotte.</p>
 				</div>
-			</div>
+			</motion.div>
 		</div>
 	);
 }
