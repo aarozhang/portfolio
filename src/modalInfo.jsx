c46262865deb939a9b75f134772947cffa42233e
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2022-01-07 10:54:13 -0500

    Create modalInfo.jsx

---
+++ src/modalInfo.jsx
@@ -0,0 +1,49 @@
+let modalInfo = {
+	Vanguard: [
+		"Owned, demoed, and developed asynchronous data validation logic for two APIs, ensuring 100% data return accuracy.",
+		"Pursued Agile and AWS trainings to develop personal knowledge.",
+		"Modernized an internal API gateway using Kong, improving scalability.",
+		"Implemented JMeter load tests in multiple product build pipelines to enforce performance standards.",
+		"Wrote support documents detailing resources and proper emergency procedures to handle service interruptions.",
+		"Established AWS multi-region availability for the team’s four APIs, drastically improving service resiliency.",
+	],
+	"UNC Grounds Department": [
+		"Developed in a 4-man team to deliver a training website containing lesson delivery, test taking, and student management features.",
+		"Built the student, login, and landing webpages while also making major contributions to the instructor page.",
+		"Designed the project's NoSQL database schema.",
+		"Managed client logistics and wrote documentation.",
+	],
+	RENCI: [
+		"Owned and designed a Google Add-On to email meeting action items, decreasing meeting time by up to ten hours per week.",
+		"Led project communication by organizing client meetings, managing task distribution, and presenting the team’s final product to program coordinators.",
+	],
+	"Second Main Phase": [
+		"Redesigned the non-technical employee onboarding system by creating presentations/diagrams to explain development features.",
+		"Automated two data reports to send medical provider activity every morning, increasing management efficiency.",
+		"Updated UI features to provide a more convenient user experience.",
+	],
+	"B.A. in Computer Science": [
+		"Object Oriented Programming",
+		"Design Patterns",
+		"Data Structures",
+		"Computer Organization",
+		"Discrete Math",
+		"Algorithms",
+		"AI",
+		"Files & Databases",
+		"Modern Web Programming",
+		"User Interfaces",
+		"Software Engineering Lab",
+	],
+	"Minor in Music": [
+		"Symphony Band",
+		"Wind Ensemble",
+		"Advanced Brass Lessons",
+		"Symphony Orchestra",
+		"Foundations in Music",
+		"Fundamentals of Music I",
+		"Music and Politics",
+	],
+};
+
+export default modalInfo;
