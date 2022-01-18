52d1909fa39bbf3876adbb93d34cf9e64603fb91
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2022-01-18 16:59:28 -0500

    adjusting welcome section margins

--- src/components/Title/title.jsx
+++ src/components/Title/title.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 function Title() {
 	return (
-		<div class="container col-xxl-8 px-4 py-5 mt-5 mb-2" id="Welcome">
+		<div class="container col-xxl-8 px-4 py-5 mt-5 mb-5" id="Welcome">
 			<motion.div
 				class="row flex-lg-row-reverse align-items-center g-5 py-5"
 				initial={{ opacity: 0 }}
